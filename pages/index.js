062d03b1e9dadf0e16c7656e57f0156e6ba938f0
Sadan Efe <69189063+kelboindaserver@users.noreply.github.com>
2022-11-29 03:47:17 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,78 +15,60 @@ function HomePage() {
             exit={{ y: -5, opacity: 0 }}
             transition={{ duration: 0.4 }}
         >
-<<<<<<< HEAD
-            <>
-                <div className="bg">
-                    <div className="site-4xl-container mx-5 lg:mx-auto flex flex-col md:flex-row items-center">
-                        <div className="max-w-2xl m-6 border-2 rounded-xl p-2 bg-zinc-100 bg-opacity-20  md:basis-1/2">
-                            <img className="rounded-xl saturate-50 hover:saturate-100 transition" src="/me-portrait-min.JPG" alt=""/>
-                        </div>
-                        <div className="space-y-4 mx-5 md:mx-0 md:basis-1/2 text-center md:text-left ">
-=======
-    <>
-        <div className="bg">
-        <div className="site-4xl-container mx-5 lg:mx-auto flex flex-col md:flex-row items-center">
-            <div className="max-w-2xl m-6 border-2 rounded-xl p-2 bg-zinc-100 bg-opacity-20  md:basis-1/2">
-                <img className="rounded-xl saturate-50 hover:saturate-100 transition" src="/me-portrait.JPG" alt=""/>
-            </div>
-            <div className="space-y-4 mx-5 md:mx-0 md:basis-1/2 text-center md:text-left ">
->>>>>>> parent of aad1a33 (pics-update)
 
-                            <h1 className="raleway-light text-3xl text-light-blue">
-                                Hello. I'm Sadan Efe.
-                            </h1>
-                            <h2 className="text-2xl raleway-md text-semi-blue">
-                                I'm studying Software Engineering at Istinye University.
-                            </h2>
-                        </div>
+            <div className="site-4xl-container">
+                <div className="flex flex-col lg:flex-row items-center">
+                    <div id="img" className="border-white border-2 rounded-2xl shadow-2xl shadow-zinc-700">
+                        <img className="max-w-sm rounded-2xl saturate-50 hover:saturate-100 transition" src="/me-portrait.JPG" alt=""/>
                     </div>
-                    <div className="site-4xl-container space-y-4 m-8 lg:mx-auto">
-                        <p className="text-lg text-normal-blue raleway-semibold">
-                            I have HTML and CSS certificate from Yıldız Teknik University and created Websites.
-                            I improved myself on Adobe products. I can design in Photoshop and edit video in Premiere Pro.
-                        </p>
-                        <p className="text-lg text-normal-blue raleway-semibold">
-                            I love playing basketball, reading technology news and playing video games with friends.
-                            I enjoy creating or learning new things.
-                        </p>
-                        <p className="text-lg text-normal-blue raleway-semibold">
-                            I'm trying do Graffiti and Illustration for personal.
-
-                            <br/>
-                            You can check it on
-                            <Link href="/art">
-                                <a className="underline mx-1 underline-offset-2 hover:text-dark-blue
-                 ease-in-out duration-100">
-                                    Art
-                                </a>
-                            </Link>
-                            page.
-                        </p>
-                    </div>
-                    <div className=" site-container flex items-center justify-around mt-10 mb-20 raleway-semibold">
-                        <Link href="https://www.instagram.com/sefeoz/">
-                            <a target="_blank" className="flex items-center text-light-blue hover:text-dark-blue ease-in-out duration-200">
-                                <FontAwesomeIcon icon={faInstagram} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
-                                Instagram
-                            </a>
-                        </Link>
-                        <Link href="https://twitter.com/sefeoz">
-                            <a target="_blank" className="flex items-center text-light-blue hover:text-dark-blue ease-in-out duration-200">
-                                <FontAwesomeIcon icon={faTwitter} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
-                                Twitter
-                            </a>
-                        </Link>
-                        <Link href="https://github.com/kelboindaserver">
-                            <a target="_blank" className="flex items-center text-light-blue hover:text-dark-blue ease-in-out duration-200">
-                                <FontAwesomeIcon icon={faGithub} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
-                                GitHub
-                            </a>
-                        </Link>
-
+                    <div className="text-center lg:text-start mx-5">
+                        <h1 className="text-3xl text-gray-200 mt-5 lg:mt-0 mb-2 raleway-light">Hello. I'm Sadan Efe.</h1>
+                        <h2 className="text-2xl raleway-md text-gray-300"> I'm studying Software Engineering at Istinye University.</h2>
                     </div>
                 </div>
-            </>
+                <div className={"mt-7 text-xl raleway-semibold text-gray-400"}>
+                    <p>I have HTML and CSS certificate from Yıldız Teknik University and created Websites.
+                        I improved myself on Adobe products. I can design in Photoshop and edit video in Premiere Pro.</p>
+                    <p className="mt-3 text-xl raleway-semibold text-gray-400">
+                        I love playing basketball, reading technology news and playing video games with friends.
+                        I enjoy creating or learning new things.
+                    </p>
+                    <p className="mt-3 text-xl raleway-semibold text-gray-400">
+                        I'm trying do Graffiti and Illustration for personal.
+
+                        <br/>
+                        You can check it on
+                        <Link href="/art">
+                            <a className="underline mx-1 underline-offset-2 hover:text-zinc-600
+                 ease-in-out duration-100 inline">
+                                Art
+                            </a>
+                        </Link>
+                        page.
+                    </p>
+                </div>
+                <div className=" site-container flex items-center justify-around mt-10 mb-20 raleway-semibold">
+                    <Link href="https://www.instagram.com/sefeoz/">
+                        <a target="_blank" className="flex items-center text-light-blue hover:text-gray-600 ease-in-out duration-200">
+                            <FontAwesomeIcon icon={faInstagram} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
+                            Instagram
+                        </a>
+                    </Link>
+                    <Link href="https://twitter.com/sefeoz">
+                        <a target="_blank" className="flex items-center text-light-blue hover:text-gray-600 ease-in-out duration-200">
+                            <FontAwesomeIcon icon={faTwitter} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
+                            Twitter
+                        </a>
+                    </Link>
+                    <Link href="https://github.com/kelboindaserver">
+                        <a target="_blank" className="flex items-center text-light-blue hover:text-gray-600 ease-in-out duration-200">
+                            <FontAwesomeIcon icon={faGithub} className="text-3xl md:mr-3 mr-2"></FontAwesomeIcon>
+                            GitHub
+                        </a>
+                    </Link>
+
+                </div>
+            </div>
         </motion.div>
     )
 }

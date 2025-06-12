576242cd6d050919cba693df22e679cd1dc07e13
Sadan Efe OZ <69189063+sefeoz@users.noreply.github.com>
2025-06-12 15:04:46 +0300

    daietapp

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,30 +3,34 @@ import Image from "next/image";
 import Link from "next/link";
 const projects = [
   {
+    title: "DAIET APP",
+    description:
+      "Built a smart diet app that helps people figure out how much they should eat based on their age, weight, and height. It gives daily calorie targets, food suggestions, and even some motivational vibes using OpenAI. Used Firebase for user login and data, and built the whole thing with React Native (or Next.js) on the front and Node.js on the back. Basically, it’s like having a chill personal trainer in your pocket.",
+    image: "/daietapp.webp",
+    link: "https://github.com/kelboindaserver/nart-dev",
+    link2: "https://daiet.app",
+  },
+  {
     title: "FK TEXTILE",
-    description: "A modern and dynamic corporate website developed for a textile company, featuring Sanity.io, Framer Motion, and TawkTo integrations.",
-    image: "/fktekstil.webp", 
+    description:
+      "A modern and dynamic corporate website developed for a textile company, featuring Sanity.io, Framer Motion, and TawkTo integrations.",
+    image: "/fktekstil.webp",
     link: "https://github.com/mach2furkan/FK-TEKSTIL-website",
     link2: "https://fktekstil.com",
   },
   {
     title: "MODA DORA",
-    description: "A corporate website developed for a modern and minimalist fashion company.",
-    image: "/modadora.webp", 
+    description:
+      "A corporate website developed for a modern and minimalist fashion company.",
+    image: "/modadora.webp",
     link: "https://github.com/kelboindaserver/modadora",
     link2: "https://modadora.com.tr",
   },
   {
-    title: "NART DEVELOPER",
-    description: "The website for our software company initiative, developed collaboratively with university colleagues to showcase our vision and services.",
-    image: "/nartdev.webp", 
-    link: "https://github.com/kelboindaserver/nart-dev",
-    link2: "https://nartdeveloper.com",
-  },
-  {
     title: "HIZIR VİNÇ",
-    description: "A professional website for our family-owned crane rental company, featuring our services, fleet information, and contact details for construction and industrial lifting solutions.",
-    image: "/hizirvinc.webp", 
+    description:
+      "A professional website for our family-owned crane rental company, featuring our services, fleet information, and contact details for construction and industrial lifting solutions.",
+    image: "/hizirvinc.webp",
     link: "https://github.com/kelboindaserver/hizirweb",
     link2: "https://hizirvinc.com",
   },
@@ -52,11 +56,27 @@ export default function Projects() {
               />
             </div>
             <div className="p-4">
-              <Link href={project.link} target="_blank" rel="noopener noreferrer" className="block">
-                <h3 className="text-xl work-medium text-background dark:text-foreground">{project.title}</h3>
+              <Link
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block"
+              >
+                <h3 className="text-xl work-medium text-background dark:text-foreground">
+                  {project.title}
+                </h3>
               </Link>
-              <Link href={project.link2} target="_blank" rel="noopener noreferrer" className="text-lg text-background dark:text-foreground work-thin">View Website</Link>
-              <p className="mt-2 text-background dark:text-foreground work-thin">{project.description}</p>
+              <Link
+                href={project.link2}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg text-background dark:text-foreground work-thin"
+              >
+                View Website
+              </Link>
+              <p className="mt-2 text-background dark:text-foreground work-thin">
+                {project.description}
+              </p>
             </div>
           </div>
         ))}
